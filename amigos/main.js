73212a1820be3c470e56cdcba7a8dd9129d0806c
Harry211201 <116766787+Harry211201@users.noreply.github.com>
2022-11-21 14:32:23 -0500

    Se añadio interaccion JS y Envio de formulario

--- amigos/main.js
+++ amigos/main.js
@@ -1,6 +1,6 @@
 const slider = document.getElementById("slider");
 let sliderSection = document.querySelectorAll(".slider-section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
 
 const btnLeft = document.getElementById("btn-left");
 const btnRight = document.getElementById("btn-right");
@@ -9,40 +9,40 @@ slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 
 /*Boton de la derecha*/
-function Next(){
+function Next() {
     let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
-    setTimeout(function(){
+    setTimeout(function () {
         slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
-        slider.style.marginLeft= "-100%";
-    },500);
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
 }
 
 /*Boton de la izquierda*/
-function Prev(){
+function Prev() {
     let sliderSection = document.querySelectorAll(".slider-section");
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
     slider.style.marginLeft = "0%";
     slider.style.transition = "all 0.5s";
-    setTimeout(function(){
+    setTimeout(function () {
         slider.style.transition = "none";
         slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-        slider.style.marginLeft= "-100%";
-    },500);
+        slider.style.marginLeft = "-100%";
+    }, 500);
 }
 
-btnRight.addEventListener('click',function(){
+btnRight.addEventListener('click', function () {
     Next();
 })
-btnLeft.addEventListener('click',function(){
+btnLeft.addEventListener('click', function () {
     Prev();
 })
 
-setInterval(function(){
+setInterval(function () {
     Next();
-},3000);
+}, 3000);
 
 
 
@@ -52,34 +52,65 @@ let rsultado = document.getElementById("resultado");
 
 function replace1() {
     let img1 = document.getElementById("img1").getAttribute('src');
-    resultado.setAttribute('src',img1);
+    resultado.setAttribute('src', img1);
 }
 function replace2() {
     let img2 = document.getElementById("img2").getAttribute('src');
-    resultado.setAttribute('src',img2);
+    resultado.setAttribute('src', img2);
 }
 function replace3() {
-    let img3 = document.getElementById("img3").getAttribute('src');    
-    resultado.setAttribute('src',img3);
+    let img3 = document.getElementById("img3").getAttribute('src');
+    resultado.setAttribute('src', img3);
 }
 function replace4() {
-    let img4 = document.getElementById("img4").getAttribute('src');    
-    resultado.setAttribute('src',img4);
+    let img4 = document.getElementById("img4").getAttribute('src');
+    resultado.setAttribute('src', img4);
 }
 
 
 
 
-img1.addEventListener('click',function(){
+img1.addEventListener('click', function () {
     replace1();
 })
-img2.addEventListener('click',function(){
+img2.addEventListener('click', function () {
     replace2();
 })
-img3.addEventListener('click',function(){
+img3.addEventListener('click', function () {
     replace3();
 })
-img4.addEventListener('click',function(){
+img4.addEventListener('click', function () {
     replace4();
 })
 
+//formulario
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Gracias por Registrarte!";
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "¡Ups! Hubo un problema al enviar su formulario"
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "¡Ups! Hubo un problema al enviar su formulario"
+    });
+}
+form.addEventListener("submit", handleSubmit)
